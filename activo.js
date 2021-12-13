55b619e100d92178072574b8cfae053d74116737
UO250788 <96045010+UO250788@users.noreply.github.com>
2021-12-13 08:56:41 +0100

    Commit inicial
    Commit inicial setup sitio

---
+++ activo.js
@@ -0,0 +1,3 @@
+var title = document.querySelector('title');
+var metaElement = document.querySelector('[title="'+title.innerHTML+'"]');
+metaElement.setAttribute("style", "color: #ffffff; background-color: #ff5500;")
---
+++ buscador.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	
+	var pags = ["index.html", "padel.html", "aficiones.html", "capgemini.html"];
+	
+	function buscar(e){
+		e.preventDefault();
+		var p = $("#textoBusca").val();
+		cargar(p);
+	}
+	
+	function cargar(pal){
+		
+		var main = $('main');
+		main.empty();
+		
+		function addResult(result){
+			console.log(result.url)
+			var temp =
+			main.append('<p><a href="http://156.35.98.100:82/'+result.url+'">' + result.title + '</a></p>');
+		}
+		
+		pags.forEach(function (pag){
+			$.get(
+				pag,function(res){
+					var parser = new DOMParser();
+					var dochtml = parser.parseFromString(res,"text/html");
+					
+					var title = dochtml.querySelector('title');
+					var metaDescription = dochtml.querySelector('meta[name="description"]');
+					var contenido = dochtml.querySelector('main').textContent;
+                    
+					if(contenido.toLowerCase().includes(pal.toLowerCase())){
+						addResult({
+							title: title.textContent,
+							url: pag,
+							descripcion: metaDescription.getAttribute('content')
+						});
+					}
+					
+				},
+				"html"
+			)
+		});
+	}
+	$("#buscador").submit(buscar);
+});
