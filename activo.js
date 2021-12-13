271670bba244ff98011788e99f7dbaa82bd8703c
UO250788 <96045010+UO250788@users.noreply.github.com>
2021-12-13 09:01:32 +0100

    Modificacion Estilo

--- activo.js
+++ activo.js
@@ -1,3 +1,3 @@
 var title = document.querySelector('title');
 var metaElement = document.querySelector('[title="'+title.innerHTML+'"]');
-metaElement.setAttribute("style", "color: #ffffff; background-color: #ff5500;")
+metaElement.setAttribute("style", "color: #ffffff; background-color: #ff2200;")
